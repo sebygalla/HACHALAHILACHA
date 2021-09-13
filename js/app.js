89ce880bd7f9f83b0145a667b24d03701c75cc5c
sebygalla <sebygalla@hotmail.com>
2021-09-12 22:07:00 -0300

    paralax (nosotros)
    paralax con js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,21 @@ const navEnlace = document.querySelector('.lista-enlaces');
 navToggle.addEventListener("click", () => {
     navEnlace.classList.toggle("lista-enlaces_visible");
 });
+
+
+
+/*Parallax*/
+
+window.onload = () => {
+    let image = document.querySelector('.img_nosotros');
+
+
+    window.addEventListener('scroll', (e) => {
+        let scroll = window.scrollY;
+
+        image.style.backgroundPositionY = `-${scroll/3}px`
+    })
+
+
+
+}
